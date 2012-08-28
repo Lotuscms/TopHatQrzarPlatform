67dc49a4c56399bccf1b0bef9fba5faa8bcf8f3f
specialkbyte <eire32kevin@gmail.com>
2012-08-28 16:05:01 +0100

    Removed unneeded code in the DomainObject Model

--- Model/domainobject.py
+++ Model/domainobject.py
@@ -1,5 +1,4 @@
 from abc import ABCMeta, abstractmethod
-from sets import Set
 from Model.Mapper.objectwatcher import ObjectWatcher as OW
 from Model.domainexception import DomainException
 
@@ -8,7 +7,6 @@ class DomainObject(object):
 	__metaclass__ = ABCMeta
 
 	_id = -1				# setup deafult id outside db storage range
-	_attr = []				# List of attributes
 
 	def __init__(self, id_=None):
 		# check valid id value (pos int)
@@ -48,10 +46,6 @@ class DomainObject(object):
 			raise DomainException("The id must be greater than one")
 
 		self._id = value
-
-	def _notifyAccess(self):
-		"""Can be overridden by child classes to provide lazy load for DomainObjects"""
-		pass
 
 	@abstractmethod		
 	def dict(self, depth=0):
