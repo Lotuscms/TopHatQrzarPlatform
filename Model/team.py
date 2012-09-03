8c1481d5d4d301804f216c620b5a94d07b2e9248
specialkbyte <eire32kevin@gmail.com>
2012-09-03 15:46:51 +0100

    Players post now works for QRzar game logic. Added addPlayer, addTeam, etc. methods to model object with referencing collection objects

--- Model/team.py
+++ Model/team.py
@@ -1,11 +1,11 @@
 from datetime import datetime
 from Model.domainobject import DomainObject
 from Model.domainexception import DomainException
-from Model.teamgame import TeamGame
 
 class Team(DomainObject):
 
 	_name = "Team"
+	_reference_code = None
 	_game = None
 	_players = None
 
@@ -21,10 +21,20 @@ class Team(DomainObject):
 
 		self._name = name
 
+	def getReferenceCode(self):
+		return self._reference_code
+
+	def setReferenceCode(self, code):
+		if len(code) is not 1:
+			raise DomainException("Reference Code is a one character code")
+
+		self._reference_code = code
+
 	def getGame(self):
 		return self._game
 
 	def setGame(self, game):
+		from Model.teamgame import TeamGame
 		if not isinstance(game, TeamGame):
 			raise DomainException("Teams can only be part of team games not %s" % str(type(game)))
 
@@ -38,6 +48,16 @@ class Team(DomainObject):
 			self._players = PM.findByTeam(self)
 			
 		return self._players
+
+	def addPlayer(self, player):
+		from Model.teamplayer import TeamPlayer
+		if not isinstance(player, TeamPlayer):
+			raise DomainException("Can only add Team Player type objects to a team not %s" % str(type(player)))
+
+		player.setTeam(self)
+
+		players = self.getPlayers()			# get players collection
+		players.add(player)					# add to the list of players
 
 	def dict(self):
 		self.getPlayers()		# hit lazy load
