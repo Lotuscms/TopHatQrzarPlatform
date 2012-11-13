b8caffd603fb1d86bf66edb4ce43996ac817e010
Colm Vize <vizec@tcd.ie>
2012-11-13 01:35:22 +0000

    fixed something that shouldn't have been changed

--- Networking/baseprotocol.py
+++ Networking/baseprotocol.py
@@ -19,34 +19,7 @@ class BaseProtocol:
 
 	@abstractmethod
 	def bind(self):
-		config=TopHatConfig().getConfig()
-		try:
-			uidNumber= getpwnam(config.User)[2] 
-			print "Dropped privileges to user '%s'. Cool pops." % config.User
-		except KeyError:
-			print "Failed to drop privileges to user '%s'. Uh-oh." % config.User
-			print "Attempting to drop to user 'nobody'"
-			try:
-				uidNumber= getpwnam('nobody')[2]
-				print "Dropped privileges to 'nobody'. Phew!"
-			except:
-				print "No user 'nobody' on this system, bit mad, I'm outta here so."
-				print "[TopHat-Service failed to start]"
-				exit(1)
-		try:
-			gidNumber= getgrnam(config.User)[2] 
-			print "Dropped privileges to group '%s'. Nice." % config.User
-		except KeyError:
-			print "Failed to drop privileges to group '%s'. :-S" % config.User
-			print "Attempting to drop to group 'daemon'"
-			try:
-				gidNumber= getgrnam('daemon')[2]
-				
-				print "Dropped privileges to group 'daemon'. It seems we're in the clear, for now."
-			except:
-				print "No group 'nobody' on this system, I'm not going to let you run me as root. Sorry."
-				print "[TopHat-Serivce failed to start]"
-				exit(1)
+			pass
 
 	@abstractmethod
 	def _registerStatusCodes(self):
